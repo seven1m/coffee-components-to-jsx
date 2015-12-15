c7afac356a9b112ec226c99f743e1dd9cbe84240
Tim Morgan <tim@timmorgan.org>
2015-12-15 16:42:36 -0600

    Improve handling of one-line tags

--- jsxify.rb
+++ jsxify.rb
@@ -27,30 +27,33 @@ class JSXify
       @out.last << '})' if @indents.last =~ /classnames$/ && @out.last !~ /\}\)$/
     when :dedent
       if (tag = line.match(/<(\w+)/))
-        @out << (' ' * count(line)) + '</' + tag[1] + '>'
+        @out << (' ' * count(line)) + '</' + tag[1] + '>'            # insert closing tag
       elsif line.match(/{$/)
-        @out << (' ' * count(line)) + "},\n"
+        @out << (' ' * count(line)) + "},\n"                         # insert closing bracket
       end
     when :line
-      line.sub!(/React\.DOM\.(\w+)/, '<\1>')
-      line.sub!(/(\s+)([A-Z]\w+)$/, '\1<\2>') # most likely a component
-      line.sub!(/> \{\},/, '>')
-      line.gsub!(/@(\w+)/, 'this.\1')
-      if @indents.last.to_s =~ /[^=]>$/ # inside a tag
-        if line =~ /^\s+'.+'$|\s+".+"$/ # literal text body
-          line.sub!(/^(\s+)'(.+)'$/, '\1\2')
-          line.sub!(/^(\s+)"(.+)"$/, '\1\2')
-        elsif line =~ /^\s+this\./ # probably a js expression body
-          line.sub!(/^(\s+)(this\..+)$/, '\1{\2}')
+      line.sub!(/React\.DOM\.(\w+)/, '<\1>')                         # a react dom element (change to a tag)
+      line.sub!(/(\s+)([A-Z][\w\.]+)$/, '\1<\2>')                    # most likely a component (change to a tag)
+      line.sub!(/> \{\},/, '>')                                      # empty brackets after a tag (remove)
+      line.gsub!(/@(\w+)/, 'this.\1')                                # change @foo to this.foo
+      if @indents.last.to_s =~ /[^=]>$/                              # inside a tag
+        if line =~ /^\s+'.+'$|\s+".+"$/                              # literal text body
+          line.sub!(/^(\s+)'(.+)'$/, '\1\2')                         # single-quoted tag content (remove quotes)
+          line.sub!(/^(\s+)"(.+)"$/, '\1\2')                         # double-quoted tag content (remove quotes)
+        elsif line =~ /^\s+this\./                                   # probably a js expression body
+          line.sub!(/^(\s+)(this\..+)$/, '\1{\2}')                   # wrap in brackets
         end
       end
-      line.sub!(/^(\s+)"(.+)"$/, '\1\2')
       line.sub!(/for (\w+), (\w+) in (.+)$/, '\3.map((\1, \2) => {') # for thing, index in things
-      line.sub!(/for (\w+) in ([^\s]+)$/, '\2.map((\1) => {') # for thing in things
-      line.sub!(/: ->$/, '() {')
-      line.sub!(/: \(([^\)]+)\)\s*->$/, '(\1) {')
-      line.sub!(/"([^"]+)"/) do
-        '`' + $1.gsub(/#\{(.+?)\}/, '${\1}') + '`'
+      line.sub!(/for (\w+) in ([^\s]+)$/, '\2.map((\1) => {')        # for thing in things
+      line.sub!(/: ->$/, '() {')                                     # function with no args
+      line.sub!(/: \(([^\)]+)\)\s*->$/, '(\1) {')                    # function with args
+      line.sub!(/"([^"]*#\{[^"]*)"/) do
+        '`' + $1.gsub(/#\{(.+?)\}/, '${\1}') + '`'                   # string interpolation
+      end
+      if line =~ /\s+<[\w\.]+> \{\s*\w+: /                           # one-line tag and props
+        line.gsub!(/(\w+): (['"`])(.+?)\2,?/, '\1=\2\3\2')           # string props
+        line.gsub!(/(\w+): ([^ ,]+),?/, '\1={\2}')                   # js expression props
       end
       if @indents.last =~ /classnames$/ && line =~ /: /
         @out.last << ', ' unless @out.last =~ /\(\{$/
@@ -64,9 +67,10 @@ class JSXify
   def phase2(op, line)
     case op
     when :line
-      if @indents.last =~ /<\w+>/ && (m = line.match(/^\s+(\w+): (.+)/)) && @out.last =~ />$/
+      if @indents.last =~ /<[\w.]+>/ && (m = line.match(/^\s+(\w+): (.+)/)) && @out.last =~ />$/
         value = m[2]
-        value = "{#{value}}" unless value =~ /\A'.*'\z/
+        value.sub!(/,$/, '')
+        value = "{#{value}}" unless value =~ /\A'.*'\z|\A".*"\z/
         @out.last.sub!(/>$/, " #{m[1]}=#{value}>")
       else
         @out << line
